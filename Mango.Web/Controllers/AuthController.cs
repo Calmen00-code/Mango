4f7e94862162106695c3baa17cec750437a45c6b
Calmen <calmenchia@gmail.com>
2025-06-20 23:11:55 +0800

    Register in Action with Role

--- Mango.Web/Controllers/AuthController.cs
+++ Mango.Web/Controllers/AuthController.cs
@@ -3,6 +3,7 @@ using Mango.Web.Service.IService;
 using Mango.Web.Utility;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Threading.Tasks;
 
 namespace Mango.Web.Controllers
 {
@@ -25,12 +26,37 @@ namespace Mango.Web.Controllers
         [HttpGet]
         public IActionResult Register()
         {
-            var roleList = new List<SelectListItem>()
+            ViewBag.RoleList = BuildDisplayRoleList();
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegistrationRequestDTO registrationRequestDTO)
+        {
+            ResponseDTO result = await _authService.RegisterAsync(registrationRequestDTO);
+            ResponseDTO assignRole;
+
+            if (result != null && result.IsSuccess)
             {
-                new SelectListItem() { Text=SD.ROLE_ADMIN, Value=SD.ROLE_ADMIN },
-                new SelectListItem() { Text=SD.ROLE_CUSTOMER, Value=SD.ROLE_CUSTOMER }
-            };
-            ViewBag.RoleList = roleList;
+                // at this point, user has been registered successfully
+
+                // now check if user did not select any role
+                // if so, default the role to Customer
+                if (string.IsNullOrEmpty(registrationRequestDTO.Role))
+                {
+                    registrationRequestDTO.Role = SD.ROLE_CUSTOMER;
+                }
+                
+                assignRole = await _authService.AssignRoleAsync(registrationRequestDTO);
+                if (assignRole != null && assignRole.IsSuccess)
+                {
+                    // successfully assign role
+                    TempData["success"] = "Registration successful";
+                    return RedirectToAction(nameof(Login));
+                }
+            }
+
+            ViewBag.RoleList = BuildDisplayRoleList();
             return View();
         }
 
@@ -38,6 +64,18 @@ namespace Mango.Web.Controllers
         public IActionResult Logout()
         {
             return View();
+        }
+
+
+        // PRIVATE METHODS
+        private List<SelectListItem> BuildDisplayRoleList()
+        {
+            var roleList = new List<SelectListItem>()
+            {
+                new SelectListItem() { Text=SD.ROLE_ADMIN, Value = SD.ROLE_ADMIN },
+                new SelectListItem() { Text=SD.ROLE_CUSTOMER, Value = SD.ROLE_CUSTOMER }
+            };
+            return roleList;
         }
     }
 }
