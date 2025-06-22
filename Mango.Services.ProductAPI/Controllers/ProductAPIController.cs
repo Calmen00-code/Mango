36df87772f5289ba2eb90a07664456d9265bf9fa
Calmen <calmenchia@gmail.com>
2025-06-22 11:02:35 +0800

    CRUD functionality for ProductAPI

--- Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -1,12 +1,131 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Mango.Services.CouponAPI.Models.DTO;
+using Mango.Services.ProductAPI.Data;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.DTO;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.ProductAPI.Controllers
 {
-    public class ProductAPIController : Controller
+    [Route("api/product")]
+    [ApiController]
+    public class ProductAPIController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly AppDbContext _db;
+        private ResponseDTO _response;
+        private IMapper _mapper;
+
+        public ProductAPIController(AppDbContext db, IMapper mapper)
         {
-            return View();
+            _db = db;
+            _mapper = mapper;
+            _response = new ResponseDTO();
+        }
+
+        [HttpGet]
+        public ResponseDTO Get()
+        {
+            try
+            {
+                IEnumerable<Product> products = _db.Products.ToList();
+                _response.Result = _mapper.Map<IEnumerable<ProductDTO>>(products);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpGet]
+        [Route("{id:int}")]
+        public ResponseDTO Get(int id)
+        {
+            try
+            {
+                Product product = _db.Products.FirstOrDefault(u => u.ProductId == id);
+                _response.Result = _mapper.Map<ProductDTO>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPost]
+        public ResponseDTO Post([FromBody] ProductDTO productDTO)
+        {
+            try
+            {
+                Product product = _mapper.Map<Product>(productDTO);
+                _db.Products.Add(product);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<ProductDTO>(product);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpPut]
+        public ResponseDTO Put([FromBody] ProductDTO productDTO)
+        {
+            try
+            {
+                Product updateProduct = _db.Products.FirstOrDefault(u => u.ProductId == productDTO.ProductId);
+
+                if (updateProduct == null)
+                {
+                    throw new Exception("Product not found");
+                }
+
+                _mapper.Map(productDTO, updateProduct);
+                //updateProduct.Name = productDTO.Name;
+                //updateProduct.Price = productDTO.Price;
+                //updateProduct.Description = productDTO.Description;
+                //updateProduct.ImageUrl = productDTO.ImageUrl;
+
+                _db.Products.Update(updateProduct);
+                _db.SaveChanges();
+
+                _response.Result = _mapper.Map<ProductDTO>(updateProduct);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
+        }
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public ResponseDTO Delete(int id)
+        {
+            try
+            {
+                Product product = _db.Products.FirstOrDefault(u => u.ProductId == id);
+                _db.Products.Remove(product);
+                _db.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.Message = ex.Message;
+            }
+
+            return _response;
         }
     }
 }
