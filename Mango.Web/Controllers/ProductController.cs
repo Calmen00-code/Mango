3294c6ddff8bea70bfa8dcb1fcb445a8ab5b5ca7
Calmen <calmenchia@gmail.com>
2025-06-22 17:04:07 +0800

    Added home page UI to display the product lists

--- Mango.Web/Controllers/ProductController.cs
+++ Mango.Web/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Mango.Web.Models;
 using Mango.Web.Service.IService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
--- Mango.Web/Models/ProductDTO.cs
+++ Mango.Web/Models/ProductDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Mango.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Web.Models
 {
     public class ProductDTO
     {
@@ -13,6 +15,9 @@
         public string CategoryName { get; set; }
 
         public string ImageUrl { get; set; }
+
+        [Range(1,100)]
+        public int Count { get; set; } = 1;
 
     }
 }
