bfec3f8a5594e69b5a3c0ff7f06e9948bdde7225
Calmen <calmenchia@gmail.com>
2025-07-15 22:48:32 +0800

    OrderAPI constant

--- Mango.Services.OrderAPI/Utility/SD.cs
+++ Mango.Services.OrderAPI/Utility/SD.cs
@@ -3,5 +3,15 @@
     public class SD
     {
         public static string ProductAPIBase { get; set; }
+
+        public const string STATUS_PENDING = "PENDING";
+        public const string STATUS_APPROVED = "APPROVED";
+        public const string STATUS_READY_FOR_PICKUP = "READY_FOR_PICKUP";
+        public const string STATUS_COMPLETED = "COMPLETED";
+        public const string STATUS_REFUNDED = "REFUNDED";
+        public const string STATUS_CANCELLED = "CANCELLED";
+
+        public const string ROLE_ADMIN = "ADMIN";
+        public const string ROLE_CUSTOMER = "CUSTOMER";
     }
 }
